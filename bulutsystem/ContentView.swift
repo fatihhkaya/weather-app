be74af07c03a21a425079833ac3b8c344a903e49
Fatih Kaya <fatih.55kaya.1@gmail.com>
2024-05-03 11:06:58 +0300

    Initial Commit

---
+++ bulutsystem/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bulutsystem
+//
+//  Created by Fatih on 3.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bulutsystem/bulutsystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  bulutsystemApp.swift
+//  bulutsystem
+//
+//  Created by Fatih on 3.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct bulutsystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
